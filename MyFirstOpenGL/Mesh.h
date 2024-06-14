a0a8a6bd30a74d187f4c4d549d98ee22bb66433f
Eric-Maestre <eric.maestre@enti.cat>
2024-06-15 00:12:17 +0200

    mesh class actualizada y funcional

--- MyFirstOpenGL/Mesh.h
+++ MyFirstOpenGL/Mesh.h
@@ -2,12 +2,20 @@
 #define MESH_H
 
 #include <vector>
+#include <string>
 #include <GL/glew.h>
+#include <glm.hpp>
 
 class Mesh {
 public:
+
+    Mesh() = default;
+
     Mesh(const std::vector<float>& vertexs, const std::vector<float>& uvs, const std::vector<float>& normals);
     void Render() const;
+
+    static Mesh LoadOBJMesh(const std::string& filePath);
+
 
 private:
     GLuint VAO, VBO, uvVBO, normalsVBO;
